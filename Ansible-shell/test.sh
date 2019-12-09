75e78b6a69b018ab621c3ce6a4c5e696abb369ff
somesh kumar <someshkusampudi@gmail.com>
2019-12-09 21:36:22 +0530

    shell playbook

---
+++ Ansible-shell/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this script is a test script which is used to create files and folders on the target machine 
+
+
+mkdir sample-folder
+cd sample-folder
+
+touch 123 abc
+
+echo "Hello world, this a sample conetent" >> abc
+
+
